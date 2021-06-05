1239710e523bf93be8b3c62bcf57c27b5e9807a2
LomAndry <lomandri21@gmail.com>
2021-06-06 01:58:56 +0400

    Cleaning and created second test

--- ModTest/DEQueue.cs
+++ ModTest/DEQueue.cs
@@ -6,7 +6,68 @@ using System.Threading.Tasks;
 
 namespace ModTest
 {
-    class DEQueue
+    class DEQueue<T>
     {
+        private List<T> items;
+
+        public DEQueue()
+        {
+            items = new List<T>();
+        }
+
+        public void pushBack(T item)
+        {
+            items.Add(item);
+        }
+
+        public void pushFront(T item)
+        {
+            items.Insert(0, item);
+        }
+
+        public T popFront()
+        {
+            T item = items.FirstOrDefault<T>();
+            if (items.Count > 0)
+            {
+                items.RemoveAt(0);
+            }
+            return item;
+        }
+
+        public T popBack()
+        {
+            T item = items.LastOrDefault<T>();
+            if (items.Count > 0)
+            {
+                items.RemoveAt(items.Count - 1);
+            }
+            return item;
+        }
+
+        public T back()
+        {
+            return items.LastOrDefault<T>();
+        }
+
+        public T front()
+        {
+            return items.FirstOrDefault<T>();
+        }
+
+        public int size()
+        {
+            return items.Count;
+        }
+
+        public void clear()
+        {
+            items.Clear();
+        }
+
+        public T[] toArray()
+        {
+            return items.ToArray();
+        }
     }
 }
