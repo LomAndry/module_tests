58babacb971b7f95ddbda9b24e56e63fcc8a2b2a
LomAndry <lomandri21@gmail.com>
2021-06-06 01:13:11 +0400

    Create and modified MyIntTest class

---
+++ MyIntTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class MyIntTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
